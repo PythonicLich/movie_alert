3ccbc6204018bf785f4ee884e21e645e6f661f0f
Joseph Sprumont <jjo.sprumont@gmail.com>
2023-09-16 17:09:52 +0200

    a lot of work done on the scraping

--- allocine.py
+++ allocine.py
@@ -2,50 +2,54 @@ import requests_html
 import re
 import css
 import json
-
-container_page_urls = []
-container_movie_urls = []
-container_movie_info = []
+import datetime
 
 
-def initialization():
-    session = requests_html.HTMLSession()
-    page = session.get(css.values["start_crawl_page"])
-    movie_links = page.html.find(css.values["css_movie_url"])
-    movie_urls = [list(x.absolute_links)[0] for x in movie_links if x not in container_movie_urls]
-    other_movies = page.html.find(css.values["css_crawl_more"])
-    other_movie_urls = [list(x.absolute_links)[0] for x in other_movies if x not in container_page_urls]
-    session.close()
-    return movie_urls, other_movie_urls
-
-
-def movie_page_scraping(url, existing_session=None):
+def initialization(url, existing_session=None):
     if existing_session is None:
         session = requests_html.HTMLSession()
     else:
         session = existing_session
-    home_page = session.get(url).html
+    page = session.get(url)
+    movie_links = page.html.find(css.values["css_movie_url"])
+    movie_urls = [list(x.absolute_links)[0] for x in movie_links]
+    other_pages = page.html.find(css.values["css_crawl_more"])
+    other_pages_urls = [list(x.absolute_links)[0] for x in other_pages]
+    if existing_session is not None:
+        session.close()
+    return movie_urls, other_pages_urls
 
-    casting_url = home_page.find(css.values["css_selector_casting_url"], first=True)
-    critic_url = home_page.find(css.values["css_public_critic_url"], first=True)
-    public_url = home_page.find(css.values["css_pro_critic_url"], first=True)
+
+def movie_page_scraping(movie_url, existing_session=None):
+    if existing_session is None:
+        session = requests_html.HTMLSession()
+    else:
+        session = existing_session
+    home_page = session.get(movie_url).html
+
+    casting_url = [x for x in home_page.absolute_links if "casting" in x][0]
+    critic_url = [x for x in home_page.absolute_links if "/critiques/presse/" in x][0]
+    public_url = [x for x in home_page.absolute_links if "/critiques/spectateurs/" in x][0]
 
     casting_page = session.get(casting_url).html
     critic_page = session.get(critic_url).html
     public_page = session.get(public_url).html
 
-    title = home_page.find(css.values["css_selector_page_title"])
-    meta = home_page.find(css.values["css_selector_meta"])
-    date = re.findall(".+(?=en salle)", meta.text)[0].strip()
-    time = re.findall("/(.+?)/", meta.text)[0].strip()
-    genre = re.findall("/(.+?)/(.+)", meta.text)[0][1].strip()
+    title = home_page.find(css.values["css_page_title"], first=True).text
+    synopsis = home_page.find(css.values["css_synopsis"], first=True).text
+    meta = home_page.find(css.values["css_meta"], first=True).text
+    date = re.findall(".+(?=en salle)", meta)[0].strip()
+    time = re.findall("/(.+?)/", meta)[0].strip()
+    genre = re.findall("/(.+?)/(.+)", meta)[0][1].strip()
     directors = [x.text for x in casting_page.find(css.values["css_directors"])]
     writers = [x.text for x in casting_page.find(css.values["css_writers"])]
     actors = [x.text for x in casting_page.find(css.values["css_actors"])]
     soundtrack = [x.text for x in casting_page.find(css.values["css_sound_track"])]
+    everyone = [x.text for x in casting_page.find(css.values["css_everyone"])]
     public_note = float(public_page.find(css.values["css_note"], first=True).replace(",", "."))
     critic_note = float(critic_page.find(css.values["css_note"], first=True).replace(",", "."))
     dictionary = {"title": title,
+                  "synopsis": synopsis,
                   "date": date,
                   "time": time,
                   "genre": genre,
@@ -54,14 +58,46 @@ def movie_page_scraping(url, existing_session=None):
                   "actors": actors,
                   "soundtrack": soundtrack,
                   "public note": public_note,
-                  "critic note": critic_note}
+                  "critic note": critic_note,
+                  "everyone": everyone}
     if existing_session is not None:
         session.close()
     return dictionary
 
 
-def movie_score_scraping(url):
-    session = requests_html.HTMLSession()
-    page = session.get(url)
+def get_movie_links(existing_session=None):
+    container_page_urls_to_visit = []
+    container_movie_urls_to_visit = []
+    container_page_urls_visited = []
+    if existing_session is None:
+        session = requests_html.HTMLSession()
+    else:
+        session = existing_session
+    container_page_urls_to_visit.append(css.values["start_crawl_page"])
+    while len([x for x in container_page_urls_to_visit if x not in container_page_urls_visited]) != 0:
+        for new_link in container_page_urls_to_visit:
+            try:
+                new_movies, new_pages = initialization(new_link, session)
+                container_page_urls_visited.append(new_link)
+                container_movie_urls_to_visit.extend([x for x in new_movies if x not in container_movie_urls_to_visit])
+                container_page_urls_to_visit.extend([x for x in new_pages if
+                                                     x not in container_page_urls_to_visit and x not in container_page_urls_visited])
+            except Exception as e:
+                print("Error during the for loop: " + str(type(e)) + "  " + str(e))
 
-    session.close()
+    if existing_session is None:
+        session.close()
+    return container_movie_urls_to_visit
+
+
+if __name__ == "__main__":
+    timestamp = datetime.datetime.now().strftime("%Y %m %d - %H %m %S")
+    main_session = requests_html.HTMLSession()
+    try:
+        movie_links = get_movie_links(main_session)
+        with open(timestamp + " data.json", "w", encoding="utf-8") as f:
+            json.dump(movie_links, f)
+    except Exception as e:
+        print("Error while retrieving the links " + str(type(e)) + " " + str(e))
+
+    main_session.close()
--- css.py
+++ css.py
@@ -2,15 +2,15 @@ values = {
 "start_crawl_page" : "https://www.allocine.fr/film/aucinema/",
 "css_movie_url" : "h2.meta-title a.meta-title-link",
 "css_crawl_more" : "nav.pagination.cf div.pagination-item-holder a.button.button-md.item",
-"css_page_title" : "div.titlebar.titlebar-page div.titlebar-titl",
+"css_page_title" : "div.titlebar",
 "css_casting_url" : "a[title='Casting']",
 "css_presse_rating" : "div.rating-holder.rating-holder-3 div.rating-item div.rating-item-content div.stareval.stareval-medium.stareval-theme-default span.stareval-note",
 "css_public_rating" : "div.rating-holder.rating-holder-3 div.rating-item div.rating-item-content div.stareval.stareval-medium.stareval-theme-default span.stareval-note",
-"css_ratings" : "div.rating-item-content",
+"css_ratings" : "div.rating-item-content .stareval-note",
 "css_original_title" : "div.meta div.meta-body div.meta-body-item span.light",
 "css_synopsis" : "section#synopsis-details div.content-txt",
 "css_cities" : "div.gd-col-left section.section.ovw div.mdl-more.gd.small-crop",
-"css_date" : "div.meta div.meta-body div.meta-body-item.meta-body-info a.xXx.date.blue-link",
+"css_date" : "div.meta div.meta-body div.meta-body-item.meta-body-info .date",
 "css_meta" : "div.meta div.meta-body div.meta-body-item.meta-body-info",
 "css_directors" : "section.section.casting-director a.meta-title-link",
 "css_writers" : "div.section.casting-list-gql a.xXx.item.link",
@@ -19,5 +19,7 @@ values = {
 "css_public_critic_url" : "a[title='Critiques spectateurs']",
 "css_pro_critic_url" : "a[title='Critiques presse']",
 "css_note" : "div.big-note span.note",
-    "css_pro_critic_note": "span.stareval-note"
+    "css_pro_critic_note": "span.stareval-note",
+    "css_everyone": ".item.link"
 }
+
